09638eee864d1d10c5832ae3b9ae8f3b11e92982
jkling <jordi.kling@akquinet.de>
2026-04-30 19:12:01 +0200

    more matchers

--- lib-kotest/src/main/kotlin/io/github/admiralbiscuit/functionalerrors/kotest/FailureMatchers.kt
+++ lib-kotest/src/main/kotlin/io/github/admiralbiscuit/functionalerrors/kotest/FailureMatchers.kt
@@ -2,6 +2,8 @@
 package io.github.admiralbiscuit.functionalerrors.kotest
 
 import io.github.admiralbiscuit.functionalerrors.Failure
+import io.github.admiralbiscuit.functionalerrors.FailureCause
+import io.github.admiralbiscuit.functionalerrors.ThrowableCause
 import io.kotest.matchers.Matcher
 import io.kotest.matchers.MatcherResult
 import io.kotest.matchers.should
@@ -16,8 +18,59 @@ fun haveMessage(message: String) =
     )
   }
 
+fun haveMessageContaining(substring: String) =
+  Matcher<Failure> { failure ->
+    MatcherResult(
+      failure.message.contains(substring),
+      { "Expected message to contain \"$substring\" but was \"${failure.message}\"" },
+      { "Expected message to not contain \"$substring\" but was \"${failure.message}\"" },
+    )
+  }
+
+fun haveFailureCause(failure: Failure) =
+  Matcher<Failure> { actual ->
+    MatcherResult(
+      actual.cause == FailureCause(failure),
+      { "Expected cause to be FailureCause($failure) but was ${actual.cause}" },
+      { "Expected cause to not be FailureCause($failure)" },
+    )
+  }
+
+fun haveThrowableCause(throwable: Throwable) =
+  Matcher<Failure> { actual ->
+    MatcherResult(
+      actual.cause == ThrowableCause(throwable),
+      { "Expected cause to be ThrowableCause($throwable) but was ${actual.cause}" },
+      { "Expected cause to not be ThrowableCause($throwable)" },
+    )
+  }
+
 fun Failure.shouldHaveMessage(message: String): Failure = apply { should(haveMessage(message)) }
 
 fun Failure.shouldNotHaveMessage(message: String): Failure = apply {
   shouldNot(haveMessage(message))
+}
+
+fun Failure.shouldHaveMessageContaining(substring: String): Failure = apply {
+  should(haveMessageContaining(substring))
+}
+
+fun Failure.shouldHaveFailureCause(failure: Failure): Failure = apply {
+  should(haveFailureCause(failure))
+}
+
+fun Failure.shouldHaveThrowableCause(throwable: Throwable): Failure = apply {
+  should(haveThrowableCause(throwable))
+}
+
+fun Failure.shouldHaveFailureCause(): FailureCause {
+  val cause = this.cause
+  if (cause !is FailureCause) error("Expected cause to be a FailureCause but was $cause")
+  return cause
+}
+
+fun Failure.shouldHaveThrowableCause(): ThrowableCause {
+  val cause = this.cause
+  if (cause !is ThrowableCause) error("Expected cause to be a ThrowableCause but was $cause")
+  return cause
 }
--- lib-kotest/src/test/kotlin/io/github/admiralbiscuit/functionalerrors/kotest/FailureMatchersTest.kt
+++ lib-kotest/src/test/kotlin/io/github/admiralbiscuit/functionalerrors/kotest/FailureMatchersTest.kt
@@ -3,6 +3,8 @@ package io.github.admiralbiscuit.functionalerrors.kotest
 
 import io.github.admiralbiscuit.functionalerrors.Cause
 import io.github.admiralbiscuit.functionalerrors.Failure
+import io.github.admiralbiscuit.functionalerrors.FailureCause
+import io.github.admiralbiscuit.functionalerrors.ThrowableCause
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
@@ -10,16 +12,20 @@ import io.kotest.matchers.shouldBe
 private data class SomeFailure(override val message: String, override val cause: Cause? = null) :
   Failure
 
+private data class OtherFailure(override val message: String, override val cause: Cause? = null) :
+  Failure
+
 class FailureMatchersTest :
   FunSpec({
+    val cause = SomeFailure("cause failure")
+    val throwable = Throwable("cause throwable")
+
     context("shouldHaveMessage") {
       val failure = SomeFailure("something went wrong")
 
-      test("shouldHaveMessage passes for matching message") {
-        failure.shouldHaveMessage("something went wrong")
-      }
+      test("passes for matching message") { failure.shouldHaveMessage("something went wrong") }
 
-      test("shouldHaveMessage fails for non-matching message") {
+      test("fails for non-matching message") {
         val error = shouldThrow<AssertionError> { failure.shouldHaveMessage("something else") }
         error.message shouldBe
           "Expected message \"something else\" but was \"something went wrong\""
@@ -27,6 +33,72 @@ class FailureMatchersTest :
 
       test("shouldNotHaveMessage passes for non-matching message") {
         failure.shouldNotHaveMessage("something else")
+      }
+    }
+
+    context("shouldHaveMessageContaining") {
+      val failure = SomeFailure("something went wrong")
+
+      test("passes when message contains substring") {
+        failure.shouldHaveMessageContaining("went wrong")
+      }
+
+      test("fails when message does not contain substring") {
+        shouldThrow<AssertionError> { failure.shouldHaveMessageContaining("all good") }
+      }
+    }
+
+    context("shouldHaveFailureCause(failure)") {
+      val failure = OtherFailure("top failure", FailureCause(cause))
+
+      test("passes for matching cause") { failure.shouldHaveFailureCause(cause) }
+
+      test("fails for non-matching cause") {
+        shouldThrow<AssertionError> { failure.shouldHaveFailureCause(OtherFailure("wrong cause")) }
+      }
+    }
+
+    context("shouldHaveThrowableCause(throwable)") {
+      val failure = SomeFailure("top failure", ThrowableCause(throwable))
+
+      test("passes for matching cause") { failure.shouldHaveThrowableCause(throwable) }
+
+      test("fails for non-matching cause") {
+        shouldThrow<AssertionError> {
+          failure.shouldHaveThrowableCause(Throwable("wrong throwable"))
+        }
+      }
+    }
+
+    context("shouldHaveFailureCause()") {
+      test("returns the FailureCause when present") {
+        val failure = OtherFailure("top failure", FailureCause(cause))
+        failure.shouldHaveFailureCause() shouldBe FailureCause(cause)
+      }
+
+      test("fails when cause is a ThrowableCause") {
+        val failure = SomeFailure("top failure", ThrowableCause(throwable))
+        shouldThrow<IllegalStateException> { failure.shouldHaveFailureCause() }
+      }
+
+      test("fails when cause is null") {
+        shouldThrow<IllegalStateException> { SomeFailure("no cause").shouldHaveFailureCause() }
+      }
+    }
+
+    context("shouldHaveThrowableCause()") {
+      test("returns the ThrowableCause when present") {
+        val failure = SomeFailure("top failure", ThrowableCause(throwable))
+        failure.shouldHaveThrowableCause() shouldBe ThrowableCause(throwable)
+      }
+
+      test("fails when cause is a FailureCause") {
+        val failure = OtherFailure("top failure", FailureCause(cause))
+        shouldThrow<IllegalStateException> { failure.shouldHaveThrowableCause() }
+      }
+
+      test("fails when cause is null") {
+        shouldThrow<IllegalStateException> { SomeFailure("no cause").shouldHaveThrowableCause() }
       }
     }
   })
